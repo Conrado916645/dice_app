d0ac045f00be3fa7c470b28a12aea734f741937c
Conrado916645 <36912212+Conrado916645@users.noreply.github.com>
2020-04-24 10:46:16 +0800

    initial app bar design

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,35 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text(
+            'Dice Game',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                  child: FlatButton(
+                    child: Image.asset('images/dice1.png'),
+                  )),
+              Expanded(
+                  child: FlatButton(
+                    child: Image.asset('images/dice1.png'),
+                  ))
+            ],
+          ),
+        ),
       ),
     );
   }
